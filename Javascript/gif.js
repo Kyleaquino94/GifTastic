74b185e1f9c916cd00e840d9bcc151bedd4c340e
Kyle Aquino <aquino.kylekay@gmail.com>
2019-10-08 18:42:05 -0400

    added an image for background, created click function for giphs to be still

--- Javascript/gif.js
+++ Javascript/gif.js
@@ -1,21 +1,108 @@
-$("button").on("click",function(){
-    var x = $(this).data("search");
+$(document).ready(function(){
 
-    var queryUrl = "https://api.giphy.com/v1/gifs/search?q=" +x+"&api_key=MqJVanYmmelhaWTdFZjzfXMnHUt3WAzb&limit=10";
-    console.log(queryUrl);
+    var displayedButtons = ["Los Angeles Lakers", "New York Knicks", "Miami Heat"];
 
-    $.ajax({
-        url: queryUrl,
-        method: "GET"
-    }).done(function(response){
-        console.log(response.data[0].rating);
-        for (var i=0; i<response.data.length; i++){
-         
-        $("#GIFArea").prepend("<p>Rating: "+response.data[i].rating + "</p>");
-        $("#GIFArea").prepend("<img src='" + response.data[i].images.downsized. url + "'>");
+    function displayImgs(){
+
+        $("#display-images").empty();
+        var input =     $(this).attr("data-name");
+        var limit =     10;
+        var queryUrl =  "https://api.giphy.com/v1/gifs/search?q=" + input + "&limit=" + limit + 
+        "&api_key=MqJVanYmmelhaWTdFZjzfXMnHUt3WAzb"; 
+
+        $.ajax({
+            url: queryUrl,
+            method: "GET"
+        }).done(function(response){
+
+            for (var k = 0; k < limit; k++) {
+
+                var displayDiv = $("<div>");
+                displayDiv.addClass("holder");
+
+                var image = $("<img>");
+                image.attr("src", response.data[k].images.original_still.url);
+                image.attr("data-still", response.data[k].images.original_still.url);
+                image.attr("data-animate", response.data[k].images.original.url);
+                image.attr("data-state", "still");
+                image.attr("class", "gif");
+                displayDiv.append(image);
+
+                var rating = response.data[k].rating;
+                console.log(response);
+                var pRating = $("<p>").text("Rating: " + rating);
+                displayDiv.append(pRating)
+
+                $("#display-images").append(displayDiv);
+
+            }
+        });
+    }
+
+
+    function renderButtons(){
+
+        $("#display-buttons").empty();
+
+        for (var i = 0; i < displayedButtons.length; i++){
+            var newButton = $("<button>") 
+            newButton.attr("class", "btn btn-default");
+            newButton.attr("id", "input")  
+            newButton.attr("data-name", displayedButtons[i]); 
+            newButton.text(displayedButtons[i]); 
+            $("#display-buttons").append(newButton); 
+
+
         }
-        
+    }
+
+
+    function imageChangeState() {
+        var state = $(this).attr("data-state");
+        var animateImage = $(this).attr("data-animate");
+        var stillImage = $(this).attr("data-still");
+
+        if(state == "still") {
+            $(this).attr("src", animateImage);
+            $(this).attr("data-state", "animate");
+        }
+
+        else if(state == "animate") {
+            $(this).attr("src", stillImage);
+            $(this).attr("data-state", "still");
+        }  
+    }
+
+    $("#submitPress").on("click", function(){
+
+        var input = $("#user-input").val().trim();
+        form.reset();
+        displayedButtons.push(input);
+                
+        renderButtons();
+
+        return false;
     })
-    
-})
+
+    renderButtons();
+
+    $(document).on("click", "#input", displayImgs);
+    $(document).on("click", ".gif", imageChangeState);
+
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
